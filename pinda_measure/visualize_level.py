38af4db78c64c42e9b4bc25017d5144ba4dd81d7
Bozo Dragojevic <bozzo@digiverse.si>
2018-11-20 23:55:32 +0100

    Show median measurement

--- pinda_measure/visualize_level.py
+++ pinda_measure/visualize_level.py
@@ -15,29 +15,19 @@ def pivot_to_mean_height(df):
     """convert X, Y, Z records to a matrix of mean Z values"""
     return pd.pivot_table(index="Y", columns="X", values="Z", data=df)
 
-def show_heatmap(df):
+def show_heatmap(df, levels=10):
     df=df[["X","Y","Z"]]
-    def span(x):
-        return max(x) - min(x)
-    g = df.groupby(["X","Y"]).agg([np.mean, span, max, min]).reset_index()
+    g = df.groupby(["X","Y"]).agg([np.median]).reset_index()
     x = list(map(float,g["X"].values))
     y = list(map(float,g["Y"].values))
-    z_mean = list(map(float,g[("Z", "mean")].values))
-    z_span = list(map(float,g[("Z", "span")].values))
-    z_min = list(map(float,g[("Z", "min")].values))
-    z_max = list(map(float,g[("Z", "max")].values))
-    f, ax = plt.subplots(2,2, sharex=True, sharey=True)
-    def tric(ax, label, d=None, txt=None):
-        z = list(map(float,g[("Z", label)].values))
-        c = ax.tricontourf(x, y, z, 20)
-        ax.plot(x, y, "ko ")
-        ax.set_title(txt or label)
-        f.colorbar(d or c, ax=ax)
-        return c
-    c00 = tric(ax[0][0], "mean")
-    c01 = tric(ax[0][1], "span", txt="span (max-min)")
-    c10 = tric(ax[1][0], "min")
-    c11 = tric(ax[1][1], "max")
+    z = list(map(float,g[("Z", "median")].values))
+    f, ax = plt.subplots(1,1, sharex=True, sharey=True)
+    cs = ax.tricontour(x, y, z, levels=levels, linewidths=0.5, colors='k')
+    ax.clabel(cs)
+    c = ax.tricontourf(x, y, z, levels=levels * 5, cmap="RdBu_r")
+    ax.plot(x, y, "ko ")
+    ax.set_title("median")
+    f.colorbar(c, ax=ax)
     f.suptitle("G30 mesh bed level probe")
 
 if __name__ == "__main__":
